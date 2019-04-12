a593a23f062beda209b5297be52487109812e84a
Joe Barnes <joebarnes333@gmail.com>
2019-04-12 11:06:08 -0700

    add logic to test addition

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,33 @@ function Pizza() {
 }
 
 
-Pizza.prototype.addItems = function(pizzaItems){
+Pizza.prototype.addItems = function (pizzaItems) {
     this.pizza.push(pizzaItems);
 }
 
+Pizza.prototype.addPrice = function(pizza){
+    for(var i =0; i < pizza.length; i++){
+        if(this.pizza[0] ==='large'){
+             this.total += largePrice;
+        }else if(this.pizza[0] ==='medium'){
+             this.total += mediumPrice;
+        }else if(this.pizza[0] ==='small'){
+            this.total += smallPrice;
+        }else if(this.pizza[1] === 'pepperoni'){
+            this.total += pepporni;
+        }else if(this.pizza[1] === 'sasuage'){
+            this.total += sasuage;
+        }else if(this.pizza[1] === 'pineapple'){
+            this.total += pinapple;
+        }
+    }
+    return this.total;
+}
+
+
 function PizzaItems(size, toppings) {
     this.size = size,
-    this.toppings = toppings
+        this.toppings = toppings
 };
 
 var largePrice = 15.00;
@@ -24,23 +44,24 @@ var pinapple = 1.50;
 
 pizza = new Pizza();
 $(function () {
-    
+
 
     $('#formOne').submit(function (event) {
         event.preventDefault();
 
     });
 
-    $('#formTwo').submit(function(event) {
+    $('#formTwo').submit(function (event) {
         event.preventDefault();
 
         var size = $('input:radio[name=size]:checked').val();
         var toppings = $('input:radio[name=topping]:checked').val();
         var newpizza = new PizzaItems(size, toppings);
         pizza.addItems(newpizza);
+        pizza.addPrice(pizza);
         console.log(pizza)
     })
-    
 
-   
+
+
 })
